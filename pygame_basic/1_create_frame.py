b16df286e054467a79cfcd1c95d2deeac344d2d2
김윤혁 <kyhyeok@gmail.com>
2021-12-30 21:45:32 +0900

    배경

--- pygame_basic/1_create_frame.py
+++ pygame_basic/1_create_frame.py
@@ -8,10 +8,16 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("PangPang")
 
+background = pygame.image.load("C:\\python\\game-pangpang\\pygame_basic\\background.png")
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    screen.blit(background, (0, 0))
+
+    pygame.display.update()
 
 pygame.quit()
