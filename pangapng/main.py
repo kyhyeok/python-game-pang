2a1edd411fa08ff3457a3846b9ce1e5220e355a1
김윤혁 <kyhyeok@gmail.com>
2021-12-31 14:00:07 +0900

    무기와 키보드 이벤트

--- pangapng/main.py
+++ pangapng/main.py
@@ -27,6 +27,17 @@ character_height = character_size[1]
 character_x_position = (screen_width / 2) - (character_width / 2)
 character_y_position = screen_height - character_height - stage_height
 
+character_to_x = 0
+character_speed = 5
+
+weapon = pygame.image.load(os.path.join(image_path, "weapon.png"))
+weapon_size = weapon.get_rect().size
+weapon_width = weapon_size[0]
+
+weapons = []
+
+weapon_speed = 10
+
 running = True
 while running:
     dt = clock.tick(30)
@@ -35,7 +46,35 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                character_to_x -= character_speed
+            elif event.key == pygame.K_RIGHT:
+                character_to_x += character_speed
+            elif event.key == pygame.K_SPACE:
+                weapon_x_position = character_x_position + (character_width / 2) - (weapon_width / 2)
+                weapon_y_position = character_y_position
+                weapons.append([weapon_x_position, weapon_y_position])
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                character_to_x = 0
+
+    character_x_position += character_to_x
+
+    if character_x_position < 0:
+        character_to_x = 0
+    elif character_x_position > screen_width - character_width:
+        character_x_position = screen_width - character_width
+
+    weapons = [[weapon[0], weapon[1] - weapon_speed] for weapon in weapons]
+    weapons = [[weapon[0], weapon[1] ] for weapon in weapons if weapon[1] > 0]
+
     screen.blit(background, (0, 0))
+
+    for weapon_x_position, weapon_y_position in weapons:
+        screen.blit(weapon, (weapon_x_position, weapon_y_position))
+
     screen.blit(stage, (0, screen_height - stage_height))
     screen.blit(character, (character_x_position, character_y_position))
 
